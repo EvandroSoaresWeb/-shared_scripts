9624d7ba88b431d9c22ecf210af8facef54fcd6b
EvandroSoaresWeb <49442589+EvandroSoaresWeb@users.noreply.github.com>
2021-06-21 13:30:23 -0300

    Update try.js

--- try.js
+++ try.js
@@ -1,38 +1,20 @@
-openMyFile()
-try {
-    function SaveData () {
+function SaveData () {
+    try {
         setCookie('id_name_lead', document.getElementById("Nome").value, 1);
         setCookie('id_user_lead', document.getElementById("Usuario").value, 1);
+   }
+    finally {
         if( navigator.userAgent.match(/Android/i)
-        || navigator.userAgent.match(/webOS/i)
-        || navigator.userAgent.match(/iPhone/i)
-        || navigator.userAgent.match(/iPad/i)
-        || navigator.userAgent.match(/iPod/i)
-        || navigator.userAgent.match(/BlackBerry/i)
-        || navigator.userAgent.match(/Windows Phone/i)
-        ){
-            window.top.location.replace("https://traderevolution.com.br/obrigado-teste-gratis-mobile/");
-        }
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)
+            ){
+                window.top.location.replace("https://traderevolution.com.br/obrigado-teste-gratis-mobile/");
+            }
         else {
             window.top.location.replace("https://traderevolution.com.br/obrigado-teste-gratis/");
         }
-    }
-   // vincula o recurso
-   writeMyFile(theData);
-}
-finally {
-    if( navigator.userAgent.match(/Android/i)
-        || navigator.userAgent.match(/webOS/i)
-        || navigator.userAgent.match(/iPhone/i)
-        || navigator.userAgent.match(/iPad/i)
-        || navigator.userAgent.match(/iPod/i)
-        || navigator.userAgent.match(/BlackBerry/i)
-        || navigator.userAgent.match(/Windows Phone/i)
-        ){
-            window.top.location.replace("https://traderevolution.com.br/obrigado-teste-gratis-mobile/");
-        }
-        else {
-            window.top.location.replace("https://traderevolution.com.br/obrigado-teste-gratis/");
-        }
-   closeMyFile(); // sempre fecha o recurso
 }
